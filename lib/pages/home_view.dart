8b0b1ef44ce86fddfc7dcf4b9130a0a3a99a717f
Mritunjay <mritunjaygupta004@gmail.com>
2023-10-21 23:20:04 +0530

    dependency injection

--- lib/pages/home_view.dart
+++ lib/pages/home_view.dart
@@ -3,7 +3,23 @@ import 'package:get/get.dart';
 import 'package:getx/my_controller.dart';
 
 class HomeView extends StatelessWidget {
-  var myController = Get.put(MyController());
+  // var myController = Get.put(MyController());
+
+  //for creating multiple instances of MyController
+  // var a = Get.put(MyController(), tag: 'abc');
+  // var b = Get.put(MyController(), tag: 'def');
+
+  //it won't be destroyed at any time
+  // var a = Get.put(MyController(), permanent: true);
+
+  // the below won't creates a new instance of MyController unless we use this instance
+  //fenix: true for lazyPut
+  // var b = Get.lazyPut(() => MyController());
+
+  //creates a new instance every time
+  // var c = Get.create(() => MyController());
+
+  // var c = Get.putAsync(() async => MyController());
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +31,16 @@ class HomeView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Obx(() => Text(
-                  "${myController.count}",
-                  style: const TextStyle(fontSize: 30),
-                )),
+            const Text(
+              "Hello",
+              style: TextStyle(fontSize: 30),
+            ),
             ElevatedButton(
                 onPressed: () {
-                  myController.increment();
+                  var abc = Get.find<MyController>(tag: 'abc');
+                  var def = Get.find<MyController>(tag: 'def');
+                  print(abc.hashCode);
+                  print(def.hashCode);
                 },
                 child: const Text('Increment'))
           ],
